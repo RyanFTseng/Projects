a537007529c26268d7639b4422f195a5b785badf
Chaitanya Nookala <chaitanya.nookala@outlook.com>
2016-07-02 14:27:27 -0700

    organized server and client into classes

--- tkinter/chatserver.py
+++ tkinter/chatserver.py
@@ -1,18 +1,10 @@
 import socket
-
-
-class socket:
+class server:
     def _init_(self,ip,port):
-        try:
             self.s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.s.bind(('127.0.0.1',10502))
-        except:
-            conn.close()
     def listen(self):
-        try:
             self.s.listen(5)
-        except:
-            conn.close()
     def accept(self):
         try:
             self.conn, addr=s.accept()
@@ -32,8 +24,8 @@ class socket:
             data= conn.recv(1024)
         except:
             conn.close()
-        
-        
+
+
 
 
 
--- tkinter/client.py
+++ tkinter/client.py
@@ -1,19 +1,18 @@
 from tkinter import*
 import socket
-host= '127.0.0.1'
-port=10502
-
 master=Tk()
 
-s=socket.socket(socket.AF_INET, socket. SOCK_STREAM)
-s.connect((host,port))
 
+class client:
+    def _init_(self,ip,port):
+        self.s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.bind(('127.0.0.1',10502))
+    def senddata(self):
+        self.s.sendall(e1.get().encode())
+    def recievedata(self):
+        data=s.recv(1024)
+        print('recieved',repr(data))
 def enter():
-    l=Label(master,text=e1.get()).grid(row=1)
-    #s.sendall(b'hello,world')
-    s.sendall(e1.get().encode())
-    data=s.recv(1024)
-    print('recieved',repr(data))
     e1.delete(0,END)
 
 e1=Entry(master)
@@ -22,4 +21,15 @@ l1=Label(master,text='').grid(row=1)
 b1=Button(master,text='enter',command=enter).grid(row=2,column=0)
 
 while True:
-    master.update()
+        master.update()
+
+
+
+
+
+
+
+
+
+
+    
