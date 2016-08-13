aec9450911723e5cdc76c49c81e038e87ee75dc5
Chaitanya Nookala <chaitanya.nookala@outlook.com>
2016-08-13 14:56:18 -0700

    update socket

--- tkinter/client2.py
+++ tkinter/client2.py
@@ -4,16 +4,18 @@ from tkinter import*
 master=Tk()
 
 host='127.0.0.1'
-port=4256
+port=8755
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect((host,port))
 
 def enter():
     l1=Label(master,text='                                                                                                                                                                                                                                                                           ').grid(row=1)
-    global x
-    x=e1.get()
-    s.send((x+'from client2:').encode())
-    l1=Label(master,text='client2:'+x).grid(row=1)
+    global Input
+    Input=e1.get()
+    s.send((Input+'from client2:').encode())
+    reserver_data=s.recv(1024)
+    print('recieved from server:',reserver_data)
+    l1=Label(master,text='client2:'+Input).grid(row=1)
     master.update()
     e1.delete(0,END)
     
--- tkinter/server.py
+++ tkinter/server.py
@@ -1,29 +1,27 @@
 import socket
 import time
 import os
-
 from threading import Thread
+from _thread import*
+
 host='127.0.0.1'
-port=4256
+port=8755
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind((host,port))
 s.listen(5)
-def clientThread():
+def serverThread():
     conn,addr=s.accept()
     print('got connection from',addr)
     while 1:
         data=conn.recv(1024)
+        
         print('get data',data)
         if not data:
             time.sleep(1)
             break
         conn.sendall(data.upper())
-        
-def myfunc(i):
-    print('sleeping 5 sec from thread', i)
-    time.sleep(1)
-    print('finished sleeping from thread')
+  
 for i in range(5):
-    t=Thread(target=clientThread)
+    t=Thread(target=serverThread)
     t.start()
 s.close()
