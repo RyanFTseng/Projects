5e3c6c47203936c7028321f27406598081a16f1b
Ryan Tseng <rtseng99@yahoo.com>
2016-12-31 09:23:40 +0000

    complete Fibonacci Numbers

--- b.py
+++ b.py
@@ -4,10 +4,17 @@ c=0
 
 print('input #')
 x=int(input())
-print(a)
-print(b)
-print(a+b)
 
-for n in range(2,x,1):
-    c=
-    print(c)
+if x==1:
+    print(a)
+if x==2:
+    print(a)
+    print(b)
+elif x>2:
+    print(a)
+    print(b)
+    for n in range(2,x,2):
+        print(a+b)
+        a=a+b
+        print(a+b)
+        b=a+b
