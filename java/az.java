1fd2440f2562c3255cb14230e6ed0405063398f4
Saffcon <rtseng99@yahoo.com>
2017-02-24 02:00:51 +0000

    do while java

---
+++ java/az.java
@@ -0,0 +1,14 @@
+class az
+{
+public static void main(String args[])
+{
+char alph;
+do
+	{
+	for( alph ='A';alph<'Z';alph++)
+	System.out.println(alph);
+	break;
+	}
+while(alph!='Z');
+   }
+}
---
+++ java/w10.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class w10
+{
+public static void main(String args[])
+{
+Scanner in =new Scanner(System.in);
+int x;
+x=0;
+{
+System.out.println("input number");
+int n;
+n=in.nextInt();
+while (x==0)
+      	{
+	if (n!=0)
+        {
+		{
+		System.out.println(n);
+		n=in.nextInt();
+		}
+	}
+	else
+	{
+	break;
+	}
+}
+}
+}
+}
