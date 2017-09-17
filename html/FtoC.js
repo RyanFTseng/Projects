522e9ccf812cfc722f66abc8802eae3ede70c9c2
Your Name <you@example.com>
2017-09-17 23:15:46 +0000

    js

---
+++ html/FtoC.js
@@ -0,0 +1,16 @@
+function FtoC(F)
+{
+	var C=(F-32)*.5556
+	return C
+}
+
+
+
+
+
+
+function CtoF(C)
+{
+	var F=(C/.5556)+32
+	return F
+}
--- html/main.js
+++ html/main.js
@@ -1,6 +1,28 @@
 var name = "Teacher";
 console.log(name);
 console.log("Teacher");
-var s = "S";
-console.log(s);
+var num = 1
+var bool = true
+var arr = [1,"bob","steve",10];
+
+
+function product(x,y)
+{
+	var p=x*y;
+	return p
+}
+
+console.log(product(4,5));
+
+function dt =new Date();
+console.log(Date());
+
+
+
+
+
+
+
+	
+
 
