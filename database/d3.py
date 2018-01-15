953e30fb64b36e930eee07e564d8f50ee42b44c0
Your Name <you@example.com>
2018-01-14 16:30:06 -0800

    db

--- database/d3.py
+++ database/d3.py
@@ -9,7 +9,16 @@ c=conn.cursor()
 #c.execute('select count(employeeid), orderid from orders group by employeeid order by count(employeeid) desc limit 8')
 #c.execute('select count(employeeid), employeeid from orders group by employeeid ')
 #c.execute('select productname from products where productid = any(selectproductid from orderdetails where quantity=10')
-c.execute('select supplierid from suppliers where exists (select price from products where price<20)')
+#c.execute('select count(customerid),country from customers group by country having count(customerid)>5')
+#c.execute('select count(customerid), country from customers group by country having count(customerid)>5 order by count(customerid) desc')
+#c.execute('select count(employeeid), employeeid from orders group by employeeid having count(employeeid)>10')
+c.execute('select count(employeeid),from orders inner join employees on orders.employeeid = employees.employeeid employees where lastname = "Davolio" group by employeeid having count(employeeid)>25')
+
+
+
+
+
+
 
 for n in c.fetchall():
     print(n)
