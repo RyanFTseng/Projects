43a91d765e472bd32a95193434ab377e3a5de8c6
Ryan Tseng <rtseng99@yahoo.com>
2020-02-07 05:46:22 +0000

    phonebook.py edited online with Bitbucket

--- tkinter/phonebook.py
+++ tkinter/phonebook.py
@@ -43,10 +43,8 @@ def show():
     e2.insert(0,d[x])
 
         
-e1=Entry(master)
-e2=Entry(master)
-e1.grid(row=1,column=1)
-e2.grid(row=2,column=1)
+e1=Entry(master).grid(row=1,column=1)
+e2=Entry(master).grid(row=2,column=1)
 l1=Label(master,text='name').grid(row=1,column=0)
 l2=Label(master,text='number').grid(row=2,column=0)
 b1=Button(master,text='add',command=add).grid(row=3,column=0)
