bc2273e2d4851876fec45abef008c6769babc067
Ryan Tseng <66948916+Saffcon@users.noreply.github.com>
2020-06-15 06:49:05 -0700

    BitBucket Setup

--- java/Ab.java
+++ java/Ab.java
@@ -1,11 +1,11 @@
 class Ab{
 int factorial(int n){
-int x=1,i;
-
-for ( i=1;i<=n;i++)
-	x=x*i;
-
-return x;
+int x=n;
+for (int i=1;i>n-1;i++){
+	n=n*(x-1);
+	x=x-i;
+}
+return(n);
 }
 
 public static void main(String args[]){
