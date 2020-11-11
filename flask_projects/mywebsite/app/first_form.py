8cf975c329fb496913ec9bec8e07d17ebecbcbad
Ryan Tseng <rtseng99@yahoo.com>
2020-11-11 02:33:15 -0800

    HTML and Flask updates

--- flask_projects/mywebsite/app/first_form.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import Form
-from wtforms import TextField, IntegerField, SubmitField
-
-class FirstForm(Form):
-    name = TextField("Enter Name")
-    age = IntegerField("Enter Age")
-    submit = SubmitField("Send")
-
